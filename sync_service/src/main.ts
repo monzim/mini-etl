e455a3de778575bc4fc82f409277dca204fad9bb
Azraf Al Monzim <dev@monzim.com>
2024-07-01 12:24:38 +0600

    feat: Update RabbitMQ transport configuration in main.ts latest
    This commit updates the RabbitMQ transport configuration in the `main.ts` file of the Sync Service. The commented out code block is replaced with a new configuration that includes the `transport` and `options` properties. This change ensures that the Sync Service uses RabbitMQ as the transport for microservice communication.
    
    Changes:
    - Uncommented the RabbitMQ transport configuration.
    - Added the `transport` and `options` properties with the appropriate values.
    
    This feature improves the communication capabilities of the Sync Service by enabling the use of RabbitMQ as the transport mechanism.

--- sync_service/src/main.ts
+++ sync_service/src/main.ts
@@ -5,13 +5,13 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(
     AppModule,
-    // {
-    //   transport: Transport.RMQ,
-    //   options: {
-    //     urls: [process.env.RABBITMQ_URL],
-    //     queue: process.env.RABBITMQ_QUEUE,
-    //   },
-    // },
+    {
+      transport: Transport.RMQ,
+      options: {
+        urls: [process.env.RABBITMQ_URL],
+        queue: process.env.RABBITMQ_QUEUE,
+      },
+    },
   );
 
   await app.listen();
